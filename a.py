1a0e9aec19671b8c388740529ce7a36c872ae877
Onlyoneariom <ariom6696@gmail.com>
2025-02-16 18:08:55 +0100

    Add files via upload

---
+++ a.py
@@ -0,0 +1,6 @@
+number = int(input('Enter your specific number'))
+print("Number to be checked:", number)
+if number%2 == 0:
+    print("You are welcome")
+else:
+    print("You are underage")
---
+++ a3.py
@@ -0,0 +1,8 @@
+import datetime
+current_time = datetime.datetime.now()
+
+print("Time now at ARIOM INTERNATIONAL ACADEMY is:", end= "")
+print(current_time)
+
+import calendar
+print("/n", calendar.calendar(2026))
